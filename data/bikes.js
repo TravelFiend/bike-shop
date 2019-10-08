aa76d36c8a8eed8e4ba6bd893c75edb080a6659d
TravelFiend <mikegrace274@gmail.com>
2019-10-07 20:42:37 -0700

    added product objects

---
+++ data/bikes.js
@@ -0,0 +1,74 @@
+const nukeproof = {
+    id: 'nukeproof',
+    name: 'Nukeproof Mega',
+    image: 'nukeproof.jpg',
+    description: 'Alloy full-suspension mountain bike.  Glide over the largest boulders. 27.5 inch wheels',
+    category: 'enduro',
+    price: 4,703.49
+}
+
+const orbea = {
+    id: 'orbea',
+    name: 'Orbea Rallon M-Team',
+    image: 'orbea.jpg',
+    description: 'For 2020, the Rallon gets a bump in travel to 160mm with a redesigned linkage assembly. 29 inch wheels',
+    category: 'enduro',
+    price: 6999.99
+}
+
+const yeti = {
+    id: 'yeti',
+    name: 'Yeti SB140',
+    image: 'yeti.jpg',
+    description: 'Designed to help you get the wheels off the ground, the SB140 has a poppy and playful feel. 27.5 inch wheels',
+    category: 'trail',
+    price: 5399.00
+}
+
+const kona = {
+    id: 'kona',
+    name: 'Kona Honzo',
+    image: 'kona.jpg',
+    description: 'The 2020 line of Honzos is classically simple, yet so incredibly capable. 29 inch wheels',
+    category: 'trail',
+    price: 4600.00
+}
+
+const colnago = {
+    id: 'colnago',
+    name: 'Colnago C64',
+    image: 'colnago.jpg',
+    description: 'The C64 frame is full and made in Italy and is the result of more than 2 years of research',
+    category: 'road',
+    price: 6199.95
+}
+
+const trek = {
+    id: 'trek',
+    name: 'Trek Domane 6.9',
+    image: 'trek.jpg',
+    description: 'IsoSpeed decoupler that isolates the seat tube from the rest of the frameset for a more comfortable riding position',
+    category: 'road',
+    price: 6930.00
+}
+
+const tricycle = {
+    id: 'tricycle',
+    name: 'Hell on Wheels',
+    image: 'tricycle.jpg',
+    description: 'Transport for only the baddest of asses',
+    category: 'Three-wheels',
+    price: 100,000
+}
+
+const bikes = [
+    nukeproof,
+    orbea,
+    yeti,
+    kona,
+    colnago,
+    trek,
+    tricycle
+];
+
+export default bikes;
--- test/render-bikes.test.js
+++ test/render-bikes.test.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import renderBikes from '../products/render-bikes.js';
 
 const test = QUnit.test;
 
