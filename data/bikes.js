33328c0bf8a631177292cd5397332ac6dc84b3a4
TravelFiend <mikegrace274@gmail.com>
2019-10-07 23:30:42 -0700

    Test and function written and passing

--- data/bikes.js
+++ data/bikes.js
@@ -4,8 +4,8 @@ const nukeproof = {
     image: 'nukeproof.jpg',
     description: 'Alloy full-suspension mountain bike.  Glide over the largest boulders. 27.5 inch wheels',
     category: 'enduro',
-    price: 4,703.49
-}
+    price: 4703.49
+};
 
 const orbea = {
     id: 'orbea',
@@ -14,7 +14,7 @@ const orbea = {
     description: 'For 2020, the Rallon gets a bump in travel to 160mm with a redesigned linkage assembly. 29 inch wheels',
     category: 'enduro',
     price: 6999.99
-}
+};
 
 const yeti = {
     id: 'yeti',
@@ -23,7 +23,7 @@ const yeti = {
     description: 'Designed to help you get the wheels off the ground, the SB140 has a poppy and playful feel. 27.5 inch wheels',
     category: 'trail',
     price: 5399.00
-}
+};
 
 const kona = {
     id: 'kona',
@@ -32,7 +32,7 @@ const kona = {
     description: 'The 2020 line of Honzos is classically simple, yet so incredibly capable. 29 inch wheels',
     category: 'trail',
     price: 4600.00
-}
+};
 
 const colnago = {
     id: 'colnago',
@@ -41,7 +41,7 @@ const colnago = {
     description: 'The C64 frame is full and made in Italy and is the result of more than 2 years of research',
     category: 'road',
     price: 6199.95
-}
+};
 
 const trek = {
     id: 'trek',
@@ -50,7 +50,7 @@ const trek = {
     description: 'IsoSpeed decoupler that isolates the seat tube from the rest of the frameset for a more comfortable riding position',
     category: 'road',
     price: 6930.00
-}
+};
 
 const tricycle = {
     id: 'tricycle',
@@ -58,8 +58,8 @@ const tricycle = {
     image: 'tricycle.jpg',
     description: 'Transport for only the baddest of asses',
     category: 'Three-wheels',
-    price: 100,000
-}
+    price: 100000.00
+};
 
 const bikes = [
     nukeproof,
--- test/render-bikes.test.js
+++ test/render-bikes.test.js
@@ -1,17 +1,29 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-import renderBikes from '../products/render-bikes.js';
+import renderBike from '../products/render-bike.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('renders a bike', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const nukeproof = {
+        id: 'nukeproof',
+        name: 'Nukeproof Mega',
+        image: './assets/nukeproof.jpg',
+        description: 'Alloy full-suspension mountain bike. Glide over the largest boulders. 27.5 inch wheels',
+        category: 'enduro',
+        price: 4703.49
+    };
 
+    const expected = '<li class="enduro" title="Alloy full-suspension mountain bike. Glide over the largest boulders. 27.5 inch wheels"><h3>Nukeproof Mega</h3><img src="./assets/nukeproof.jpg" alt="Nukeproof Mega image"><details>Alloy full-suspension mountain bike. Glide over the largest boulders. 27.5 inch wheels</details><div class="price-and-add"><div class="price">$4703.49</div><button value="nukeproof">Add to cart</button></div></li>';
+
+  // <p class="price">$1.00<button value="apple">Add</button></p></li>;
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const dom = renderBike(nukeproof);
+    const html = dom.outerHTML;
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(html, expected);
 });
