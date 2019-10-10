5348ca2e5feba1de3b0cf24b926829931dcb2c63
TravelFiend <mikegrace274@gmail.com>
2019-10-10 12:35:02 -0700

    still working on this one, not working yet

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -2,6 +2,7 @@ import bikes from '../data/bikes.js';
 import cartData from '../data/cart.js';
 import { renderTableRow } from './render-table-row.js';
 import findById, { calcOrderTotal, makePrettyCurrency } from '../common/utils.js';
+import { CART_KEY } from '../products/render-bike.js';
 
 const tableBody = document.querySelector('tbody');
 const orderTotalCell = document.getElementById('order-total-cell');
