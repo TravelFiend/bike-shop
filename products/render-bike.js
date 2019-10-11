8f4dda5a15ed2431b6d04544137dfca936d1ae0d
TravelFiend <mikegrace274@gmail.com>
2019-10-11 00:30:21 -0700

    place order button clears local storage and redirects to home page

--- products/render-bike.js
+++ products/render-bike.js
@@ -65,7 +65,6 @@ const renderBike = bike => {
         if (!localStorageCart){
             makeCart();
             localStorageCart = getCart();
-            console.log(`I made a cart`);
         }
         
         // console.log('bike id', bike.id);
@@ -73,7 +72,6 @@ const renderBike = bike => {
         // console.log('localstoargecart', localStorageCart);
         
         setCart(localStorageCart);
-        console.log(localStorageCart);
     });
     div.appendChild(myButton);
 
--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -1,11 +1,13 @@
 import bikes from '../data/bikes.js';
-// import cartData from '../data/cart.js';
+import { CART_KEY } from '../products/render-bike.js';
 import { renderTableRow } from './render-table-row.js';
 import findById, { calcOrderTotal, makePrettyCurrency } from '../common/utils.js';
 import { getCart } from '../products/render-bike.js';
 
 const tableBody = document.querySelector('tbody');
 const orderTotalCell = document.getElementById('order-total-cell');
+const orderButton = document.getElementById('order-button');
+
 let localCart = getCart();
 
 localCart.forEach(lineItem => {
@@ -19,4 +21,15 @@ let orderTotal = calcOrderTotal(localCart, bikes);
 let prettyOrderTotal = makePrettyCurrency(orderTotal);
 orderTotalCell.textContent = prettyOrderTotal;
 
+const submitButtonAction = () => {
+    if (getCart() === null){
+        orderButton.setAttribute('disabled', 'disabled');
+    }
+    orderButton.addEventListener('click', () => {
+        orderButton.setAttribute('disabled', 'disabled');
+        localStorage.removeItem(CART_KEY);
+        window.location.href = '../';
+    });
+};
 
+submitButtonAction();
